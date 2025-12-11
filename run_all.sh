2bc9f3d4e7049865ff0feb6b80ee541207f77afd
huxiaoou <516984451@qq.com>
2025-12-11 17:28:04 +0800

    Add run_all.sh;Run with end_date = '20251128'

---
+++ run_all.sh
@@ -0,0 +1,14 @@
+bgn_date="20160104"
+if [ "$#" -eq 1 ]; then
+    if [ "$1" = "--auto" ]; then
+        end_date=$(date +"%Y%m%d")
+    else
+        end_date="$1"
+    fi
+else
+    read -p "Please input the end date, format = [YYYYMMDD]:" end_date
+fi
+echo "end_date = $end_date"
+
+python main.py preprocess --bgn $bgn_date --end $end_date
+python main.py dominant --bgn $bgn_date --end $end_date
