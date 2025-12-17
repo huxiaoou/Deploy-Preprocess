98438417ad5333f0f3c7b9724d4a4b6c48f762f2
huxiaoou <516984451@qq.com>
2025-12-17 15:07:33 +0800

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,4 +1,9 @@
-bgn_date="20160104"
+#!/usr/bin/bash -l
+
+echo "$(date +'%Y-%m-%d %H:%M:%S') begins"
+udb=$(python -c $'import yaml\nwith open("config.yaml", "r") as f:_config = yaml.safe_load(f)\nprint(_config["dbs"]["user"])')
+echo "user_db=$udb"
+
 if [ "$#" -eq 1 ]; then
     if [ "$1" = "--auto" ]; then
         end_date=$(date +"%Y%m%d")
@@ -10,7 +15,21 @@ else
 fi
 echo "end_date = $end_date"
 
+rm_tqdb $udb --table preprocess
+rm_tqdb $udb --table dominant
+rm_tqdb $udb --table avlb
+rm_tqdb $udb --table mkt
+
+bgn_date="20160104"
+
 python main.py preprocess --bgn $bgn_date --end $end_date
+echo "$(date +'%Y-%m-%d %H:%M:%S') preprocess data created"
+
 python main.py dominant --bgn $bgn_date --end $end_date
+echo "$(date +'%Y-%m-%d %H:%M:%S') dominant data created"
+
 python main.py avlb --bgn $bgn_date --end $end_date
+echo "$(date +'%Y-%m-%d %H:%M:%S') avlb data created"
+
 python main.py mkt --bgn $bgn_date --end $end_date
+echo "$(date +'%Y-%m-%d %H:%M:%S') mkt data created"
